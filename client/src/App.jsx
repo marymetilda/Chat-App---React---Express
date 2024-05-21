f6cdc0ea0428a1740766d9dbf96307fcc0cff762
mary-metilda <mary.metilda@vonnue.com>
2024-05-21 14:00:40 +0530

    feat: chat page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import socketIO from "socket.io-client";
 import Home from "./components/Home";
+import ChatPage from "./components/ChatPage";
 
 const socket = socketIO.connect("http://localhost:4000");
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
