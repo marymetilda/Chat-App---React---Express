2f01c0f3b588dda5c7d07c2887bc08e689fbf21e
mary-metilda <mary.metilda@vonnue.com>
2024-05-22 18:03:03 +0530

    feat: create event listener in node js

--- server/index.js
+++ server/index.js
@@ -13,16 +13,30 @@ const socketIO = require("socket.io")(http, {
 
 app.use(cors());
 
+let users = [];
+
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
-
   //sends the message to all the users on the server
   socket.on("message", (data) => {
     socketIO.emit("messageResponse", data);
   });
 
+  //Listens when a new user joins the server
+  socket.on("newUser", (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+  });
+
   socket.on("disconnect", () => {
     console.log("🔥: A user disconnected");
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
   });
 });
 
